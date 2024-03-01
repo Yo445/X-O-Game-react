21ba58fe918c0de27ce3d067b51de62ac86396bd
Yo445 <ya108942@gmail.com>
2024-03-01 11:01:30 +0200

    test

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Bord from "./components/Bord";
+
+function App() {
+  return (
+    <div className="playground">
+      <Bord />
+    </div>
+  );
+}
+
+export default App;
